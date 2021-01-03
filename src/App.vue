d21838d75a36433af4bea71798cac921693122e5
Poojakp94 <poojakprasad94@gmail.com>
2021-01-03 19:06:03 +0530

    first vue attempt

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+  
+  <button @click="toggleModal">Click me</button>
+  <div v-if="showModal">
+  <Modal :header='header' :text="text" theme="sale" @close="toggleModal"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from './components/Modal.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal},
+  data(){
+    return {
+      title: 'something new',
+      header: 'Sign up for the giveaway!',
+      text: 'need coffee',
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
   }
+ 
+  
 }
 </script>
 
@@ -21,6 +37,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{sale: theme === 'sale'}">
+            <h1>Modal Title</h1>
+            <p>{{header}}</p>
+            <span>{{text}}</span>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['header', 'text', 'theme'],
+    methods: {
+        closeModal(){
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background-color: #fff;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top:0;
+        position: fixed;
+        background: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .modal.sale {
+        color: #fff;
+        background: crimson;
+    }
+</style>
